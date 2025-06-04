d15bd26a066909ac910416615c8c9113fb612f8a
Cancani <efekan.demirci@tbz.ch>
2025-06-04 09:30:18 +0200

    file structure changed

--- restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/sem3-template || exit
-source venv/bin/activate || python3 -m venv venv && source venv/bin/activate
-pip install -r requirements.txt
-pkill gunicorn || true
-gunicorn -w 4 -b 0.0.0.0:8000 run:app &
